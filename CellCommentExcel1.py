8f125c65e14a083206d94c4cea55dc1daf36d918
ElnurSh <elnur.shiri@gmail.com>
2022-11-14 16:25:45 +0400

    Create CellCommentExcel1.py
    Copy and insert cell comment from column A to column B.

---
+++ CellCommentExcel1.py
@@ -0,0 +1,20 @@
+# копировать примечания ячеек в пустые ячейки
+
+from openpyxl import load_workbook
+
+
+workbook = load_workbook('/Users/User/Desktop/test.xlsx')
+workbook1 = workbook.active
+first_sheet = workbook.get_sheet_names()[0]
+worksheet = workbook.get_sheet_by_name(first_sheet)
+
+lst = []
+num = 0
+
+for data in worksheet['A']:
+    lst.append(data.comment.text)
+    num += 1
+    workbook1.cell(row=num, column=2).value = lst[0]
+    lst.pop(0)
+
+workbook.save('/Users/User/Desktop/test.xlsx')
